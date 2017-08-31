ae069ac52126efba93e6c2cdd4353f4358d454de
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-08-31 12:06:09 +0100

    Fetch and display first train info

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 class App extends Component {
   apiData = { data: 'Loading...' }
+  trains = []
 
   componentDidMount() {
     var component = this
     var localhost_url = 'http://localhost:3001/'
     var cannon_street_url = 'http://transportapi.com/v3/uk/train/station/CST/live.json?type=departure&app_id=88e9b09b&app_key=b67f9c0a642d1931247492f109f1d561'
+
     fetch(cannon_street_url)
     .then((resp) => resp.json())
     .then(function(data){
@@ -25,12 +27,16 @@ class App extends Component {
       <div className="App">
         <h1>London Cannon Street</h1>
         <h2>Departures:</h2>
-        <h3>Time	Destination	Plat	Expected</h3>
+        <h3>Time - Destination - Plat - Expected</h3>
         { console.log(this.apiData.data.all) }
-
         { console.log(this.apiData.data.all ? this.apiData.data.all[0] : 'none') }
+        <div>
+        { this.apiData.data.all ? this.apiData.data.all[0].aimed_departure_time : this.apiData.data.toString() } - 
+        { this.apiData.data.all ? this.apiData.data.all[0].destination_name : this.apiData.data.toString() } -
+        { this.apiData.data.all ? this.apiData.data.all[0].platform : this.apiData.data.toString() } -
+        { this.apiData.data.all ? this.apiData.data.all[0].expected_departure_time : this.apiData.data.toString() }
 
-
+        </div>
       </div>
     );
   };
