39cb35e2a8aa8af33fe4fc9645cdfcdfc9a5e49f
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-08-31 16:06:53 +0100

    Server returns data obtained from Transport API

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  // res.render('index', { title: 'Cannon Street' });
 
-  res.json({sender: 'server', greeting: 'Hello from your API!'});
+  res.header("Access-Control-Allow-Origin", "*");
+
+  request("http://transportapi.com/v3/uk/train/station/CST/live.json?type=departure&app_id=88e9b09b&app_key=b67f9c0a642d1931247492f109f1d561", function(error, response, body) {
+    data = body;
+    console.log(data);
+  });
+  // res.render('index', { title: 'Cannon Street' });
+  res.json(data);
 
 });
 
