bfee89832072dbdce10f6c9d1fc7ddd0ffa34d80
artemis <artemisxen@gmail.com>
2017-08-31 16:40:32 +0100

    transportApi call updates automatically

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var request = require("request");
+var data = {sender: 'server', greeting: 'Hello'};
 
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", "*");
-
-  request("http://transportapi.com/v3/uk/train/station/CST/live.json?type=departure&app_id=88e9b09b&app_key=b67f9c0a642d1931247492f109f1d561", function(error, response, body) {
+setInterval(function() {
+  request("http://transportapi.com/v3/uk/train/station/CST/live.json?type=departure&app_id=" + process.env.APP_ID + "&app_key=" + process.env.APP_KEY, function(error, response, body) {
     data = body;
     console.log(data);
   });
+}, 10000);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   // res.render('index', { title: 'Cannon Street' });
   res.json(data);
-
 });
 
 module.exports = router;
